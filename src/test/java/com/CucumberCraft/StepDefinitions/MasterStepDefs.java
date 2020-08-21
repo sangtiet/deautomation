0bb3c830c549ece65b9c135ca75303a9b7b463a9
Vu Bach <bachthevu@gmail.com>
2020-08-21 23:19:04 +0700

    Vu commit

--- src/test/java/com/CucumberCraft/StepDefinitions/MasterStepDefs.java
+++ src/test/java/com/CucumberCraft/StepDefinitions/MasterStepDefs.java
@@ -1,12 +1,8 @@
 package com.CucumberCraft.StepDefinitions;
 
 import java.util.Properties;
-
 import com.CucumberCraft.SupportLibraries.SeleniumTestParameters;
-import com.CucumberCraft.SupportLibraries.TestController;
-
 import cucumber.api.Scenario;
-import cucumber.api.java.en.And;
 
 public abstract class MasterStepDefs {
 
